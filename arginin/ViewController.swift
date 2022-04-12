ea413a505d5eecab8c8a98c385e67eebd705aa23
김범 <kqw1209@gmail.com>
2022-04-12 19:52:15 +0900

    Initial Commit

---
+++ arginin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  arginin
+//
+//  Created by Beomkim on 2022/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
